7db1796993c40225821fb60c694ce4ba7e8a7347
Craig Banach <craigsb93@gmail.com>
2016-04-07 15:43:34 +0000

    Added user verification with database to recognise previously signed in users and auto-signup new users

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
+"use strict"
+
 var express = require("express");
 var router = express.Router();
 var passport = require('passport');
 var session = require('express-session');
+var userDB = require("../models/users");
 
 var app = express();
 
@@ -13,9 +16,17 @@ router.get('/login',
 router.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
-  // Successful authentication, redirect home.
-  res.send('/home');
-});
+    var userDetails;
+  // Successful authentication.
+    userDB.searchForUser(req.user.id).then(
+      function(doc) {
+        userDetails = doc;
+        res.send('Welcome, ' + userDetails[0].userName);
+      },   // When resolved
+      function(user) {userDB.addNewUser(user, req.user._json.login)}  //when rejected
+      );
+  
+  });
 
 router.get("/", function(req, res) {
   res.render("./index");
